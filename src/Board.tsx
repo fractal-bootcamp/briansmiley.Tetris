70e0bbdbc52a569c29a78f21996ddcde67cee4d1
Brian Smiley <briantsmiley42@gmail.com>
2024-06-15 18:02:29 -0400

    basic BoardComponent

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,7 +1,22 @@
-import { useState } from "react";
-import { Game, gameInit } from "./Tetris";
-const Board = ({ game }: { game: Game }) => {
-  const [gameState, setGameState] = useState(gameInit());
+import { Board } from "./Tetris";
+
+const superIndex = (arr: any[][], row: number, col: number) =>
+  row * arr[0].length + col;
+const BoardDisplay = ({ board }: { board: Board }) => {
+  return (
+    <div>
+      {board.map((r, row) =>
+        r.map((cell, col) => (
+          <div
+            className="bg-red-500 p-2"
+            key={superIndex(board, row, col).toString()}
+          >
+            Cell: {cell}
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
-export default Board;
+export default BoardDisplay;
