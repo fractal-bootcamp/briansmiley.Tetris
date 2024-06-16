074add683ad5e07a365474c17384d7bf0885b77c
Brian Smiley <briantsmiley42@gmail.com>
2024-06-16 19:36:11 -0400

    adds a key to each board cell and row

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,6 +5,10 @@ type BoardDisplayProps = {
   board: Board;
   CONFIG: Config;
 };
+
+const keyFromPosition = (a: number, b: number) =>
+  a.toString().padStart(2, "0") + b.toString().padStart(2, "0");
+
 const BoardDisplay = ({ board, CONFIG }: BoardDisplayProps) => {
   const aspectRatio = CONFIG.BOARD_WIDTH / CONFIG.BOARD_HEIGHT;
   const dimensions: React.CSSProperties = {
@@ -14,9 +18,13 @@ const BoardDisplay = ({ board, CONFIG }: BoardDisplayProps) => {
   return (
     <div className="flex flex-col" style={dimensions}>
       {board.map((r, row) => (
-        <div className="flex flex-1">
+        <div className="flex flex-1" key={row}>
           {r.map((cell, col) => (
-            <BoardCell position={[row, col]} cellValue={cell} />
+            <BoardCell
+              position={[row, col]}
+              cellValue={cell}
+              key={keyFromPosition(row, col)}
+            />
           ))}
         </div>
       ))}
