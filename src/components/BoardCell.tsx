87f927b9fdbe967893dae3282d1f09b48e5d8a3b
Brian Smiley <briantsmiley42@gmail.com>
2024-06-16 17:28:00 -0400

    makes blocks tetrisyer

--- src/components/BoardCell.tsx
+++ src/components/BoardCell.tsx
@@ -11,7 +11,7 @@ const BoardCell = ({ cellValue, position }: BoardCellProps) => {
     ((row % 2) + (col % 2)) % 2 ? "#151515" : "#101010";
   const cellDynamicStyles: React.CSSProperties = {
     background: cellValue || blankCellBackground.toString(),
-    borderWidth: cellValue ? 4 : 0,
+    borderWidth: cellValue ? 8 : 0,
     borderStyle: "outset",
     borderColor: `rgb(from ${cellValue} calc(.8*r) calc(.8*g) calc(.8*b))`
   };
