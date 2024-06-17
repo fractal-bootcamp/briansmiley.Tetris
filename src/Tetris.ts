fdcb4ba15ae4d9b5640e12655b266ba3ce4797f2
Brian Smiley <briantsmiley42@gmail.com>
2024-06-17 10:31:00 -0400

    factors out CONFIG  and types into its own file

--- src/Tetris.ts
+++ src/Tetris.ts
@@ -1,3 +1,5 @@
+import { CONFIG } from "./TetrisConfig";
+import type { Config, Coordinate, TetrisShape } from "./TetrisConfig";
 /**
  * Types
  */
@@ -22,91 +24,12 @@ type Block = {
 };
 export type Direction = "L" | "R" | "D";
 export type RotDirection = "CW" | "CCW";
-type Coordinate = [number, number];
-
-type TetrisShape = "I" | "T" | "O" | "S" | "Z" | "L" | "J";
-export type Config = {
-  BLOCK_SHAPES: Record<TetrisShape, Coordinate[]>;
-  SPAWN_POINT: Coordinate;
-  SHAPE_COLORS: Record<TetrisShape, string>;
-  BOARD_WIDTH: number;
-  BOARD_HEIGHT: number;
-  STARTING_TICK_INTERVAL: number;
-  SPEED_SCALING: number;
-  LEVEL_LINES: number; //how many lines between speed scaling
-  INPUT_REPEAT_DELAY: number;
-  WALLS: boolean;
-};
 
 type ConditionalNull<argType, nonNullArgType, returnType> =
   argType extends nonNullArgType ? returnType : null;
 /**
  * Data
  */
-
-export const CONFIG: Config = {
-  BLOCK_SHAPES: {
-    I: [
-      [0, -1],
-      [0, 0],
-      [0, 1],
-      [0, 2]
-    ],
-    T: [
-      [0, 0],
-      [0, 1],
-      [0, -1],
-      [1, 0]
-    ],
-    O: [
-      [0, 0],
-      [1, 0],
-      [0, 1],
-      [1, 1]
-    ],
-    S: [
-      [0, 0],
-      [1, 0],
-      [0, 1],
-      [1, -1]
-    ],
-    Z: [
-      [0, 0],
-      [1, 0],
-      [1, 1],
-      [0, -1]
-    ],
-    L: [
-      [0, 1],
-      [0, 0],
-      [0, -1],
-      [-1, 1]
-    ],
-    J: [
-      [0, 0],
-      [0, 1],
-      [0, -1],
-      [1, 1]
-    ]
-  },
-  SPAWN_POINT: [0, 4] as Coordinate,
-  SHAPE_COLORS: {
-    I: "#00ffff",
-    T: "#800080",
-    O: "#ffff00",
-    S: "#ff0000",
-    Z: "#00ff00",
-    L: "#ff7f00",
-    J: "#0000ff"
-  },
-  BOARD_WIDTH: 10,
-  BOARD_HEIGHT: 20,
-  STARTING_TICK_INTERVAL: 350,
-  SPEED_SCALING: 1.34, //step multiplier for game speed increase
-  LEVEL_LINES: 5,
-  INPUT_REPEAT_DELAY: 20,
-  WALLS: true
-};
 
 /**
  * Functions
