534ca6c575537999edcab409ddf9fd2eadc14010
Brian Smiley <briantsmiley42@gmail.com>
2024-06-16 20:41:30 -0400

    adds music

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BoardDisplay from "./components/Board";
+import { Volume2, VolumeX } from "lucide-react";
 import {
   Game,
   allowInput,
@@ -12,13 +13,18 @@ import {
   startGame,
   tickGravity
 } from "./Tetris";
+import ThemeSong from "./assets/ThemeSong.mp3";
 import useKeyDown from "./hooks/useKeyDown";
-/** https://medium.com/@paulohfev/problem-solving-custom-react-hook-for-keydown-events-e68c8b0a371 */
 
+const music = new Audio(ThemeSong);
+const startMusic = () => {
+  music.loop = true;
+  music.play();
+};
 function App() {
   const [gameClock, setGameClock] = useState(0);
   const [gameState, setGameState] = useState(gameInit());
-
+  const [unMuted, setMuted] = useState<boolean | null>(null);
   //each time the game clock ticks, sets the next tick to happen after a delay set by the current value of tickInterval
   useEffect(() => {
     // if (gameState.over) return; //stop ticking if the game ends
@@ -33,7 +39,9 @@ function App() {
     if (gameState.over) return;
     setGameState(tickGravity(gameState));
   }, [gameClock]);
-
+  useEffect(() => {
+    music.muted = !unMuted;
+  }, [unMuted]);
   const handleInput = (newGameState: Game) => {
     if (gameState.over || gameState.inputForbidden) return;
     setGameState(forbidInput(newGameState));
@@ -41,6 +49,11 @@ function App() {
       () => setGameState(game => allowInput(game)),
       gameState.CONFIG.INPUT_REPEAT_DELAY
     );
+  };
+  const handleSoundClick = (e: React.MouseEvent) => {
+    // e.preventDefault();
+    if (unMuted === null) startMusic();
+    setMuted(!unMuted);
   };
 
   //prettier-ignore
@@ -55,18 +68,28 @@ function App() {
   }
   return (
     <>
-      <div className="m-2 flex flex-col items-center gap-2 ">
-        <BoardDisplay
-          board={boardWithFallingBlock(gameState)}
-          CONFIG={gameState.CONFIG}
-        />
-        <div className="flex justify-center">
-          <button
-            className="btn"
-            onClick={() => setGameState(startGame(gameState))}
-          >
-            Start
-          </button>
+      <div className="flex justify-center">
+        <div className="m-2 flex flex-col items-center gap-2 w-fit">
+          <BoardDisplay
+            board={boardWithFallingBlock(gameState)}
+            CONFIG={gameState.CONFIG}
+          />
+          <div className="flex justify-between w-full">
+            <div></div>
+            <button
+              className="btn"
+              onClick={() => setGameState(startGame(gameState))}
+            >
+              Start
+            </button>
+            <div onClick={e => handleSoundClick(e)}>
+              {unMuted ? (
+                <Volume2 className="w-10 h-10" />
+              ) : (
+                <VolumeX className="w-10 h-10" />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
