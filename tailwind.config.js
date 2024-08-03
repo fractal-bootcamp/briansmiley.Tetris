550b45d864f4ac1ba5aac42befcdd067e7a044fe
Brian Smiley <briantsmiley42@gmail.com>
2024-08-03 15:45:42 -0400

    adds flash animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        flash: {
+          '0%, 100%': { opacity: '1' },
+          '20%, 80%': { opacity: '.8' },
+          '50%': { opacity: '.2' },
+        },
+      },
+      animation: {
+        flash: 'flash 1s linear infinite',
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
