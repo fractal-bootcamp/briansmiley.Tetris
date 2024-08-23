7c779ec8cd0fd445185c3747bb70271258aaab17
Brian Smiley <briantsmiley42@gmail.com>
2024-08-23 18:50:22 -0400

    gets basic touch controls!

--- src/AppMobile.tsx
+++ src/AppMobile.tsx
@@ -1,131 +1,32 @@
 import BoardDisplay from './components/BoardDisplay';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { Volume2, VolumeX } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
 import {
-  Game,
+  Direction,
   boardWithFallingBlock,
   gameInit,
   hardDropBlock,
   holdAndPopHeld,
-  miniHeldBoard,
-  miniPreviewBoard,
   rotateBlock,
-  setAllowedInput,
   shiftBlock,
   startGame,
   tickGameClock,
 } from './Tetris';
 import ThemeSong from './assets/ThemeSong.mp3';
-import useKeysPressed from './hooks/useKeysPressed';
-import { CONFIG, InputCategory } from './TetrisConfig';
-import BoardCell from './components/BoardCell';
+import { CONFIG } from './TetrisConfig';
 import PieceDisplay from './components/PieceDisplay';
+import { SwipeEventData, useSwipeable } from 'react-swipeable';
 const music = new Audio(ThemeSong);
-const startMusic = () => {
-  music.loop = true;
-  music.play();
-};
-interface KeyBinding {
-  key: string;
-  type: InputCategory;
-  callback: (prev: Game) => Game;
-}
-//prettier-ignore
-const keyBindings: KeyBinding[] = [
-  { key: "w", type: "rotate",callback: (prevGameState) => rotateBlock(prevGameState, "CW") },
-  { key: "e", type: "rotate",callback: (prevGameState) => rotateBlock(prevGameState, "CCW")},
-  { key: "a", type:"shift", callback: (prevGameState) => shiftBlock(prevGameState, "L")},
-  { key: "d", type:"shift", callback: (prevGameState) => shiftBlock(prevGameState, "R")},
-  { key: "s", type:"shift", callback: (prevGameState) => shiftBlock(prevGameState, "D")},
-  { key: " ", type: "drop", callback: (prevGameState) => hardDropBlock(prevGameState, )},
-  { key: "ArrowLeft", type:"shift", callback: (prevGameState) => shiftBlock(prevGameState, "L")},
-  { key: "ArrowRight", type:"shift", callback: (prevGameState) => shiftBlock(prevGameState, "R")},
-  { key: "ArrowUp", type: "rotate",callback: (prevGameState) => rotateBlock(prevGameState, "CW")},
-  { key: "ArrowDown", type:"shift", callback: (prevGameState) => shiftBlock(prevGameState, "D")},
-  { key: "c", type:"hold", callback: (prevGameState) => holdAndPopHeld(prevGameState)},
-];
+
 const cellBorderStyles = ['outset', 'none'];
 
 export default function MobileApp() {
   const [gameState, setGameState] = useState(gameInit());
   const gameStateRef = useRef(gameState);
   const [cellBorderStyleIndex, setCellBorderStyle] = useState(0);
-  const keysPressed = useKeysPressed(keyBindings.map((binding) => binding.key));
-  const keysPressedRef = useRef(keysPressed);
-  const currentlyShiftingRef = useRef(false);
 
-  //Sync up gamestate and keyspressed refs
-  useEffect(() => {
-    keysPressedRef.current = keysPressed;
-  }, [keysPressed]);
   useEffect(() => {
     gameStateRef.current = gameState;
   }, [gameState]);
-
-  //set up input polling
-  useEffect(() => {
-    const shiftInputLoop = setInterval(
-      processShiftInputs,
-      gameState.CONFIG.POLL_RATES.base
-    );
-    const handleKeyDowns = (e: KeyboardEvent) => {
-      keyBindings.forEach((binding) => {
-        if (
-          binding.type === 'shift' ||
-          !gameStateRef.current.allowedInputs[binding.type]
-        )
-          return; //shift is hadnled by keyspressed
-        if (e.key === binding.key) {
-          setGameState((prev) =>
-            setAllowedInput(binding.callback(prev), binding.type, false)
-          );
-        }
-      });
-    };
-    const handleKeyUps = (e: KeyboardEvent) => {
-      keyBindings.forEach((binding) => {
-        if (binding.type === 'shift') return;
-        if (e.key === binding.key) {
-          setGameState((prev) => setAllowedInput(prev, binding.type, true));
-        }
-      });
-    };
-    document.addEventListener('keydown', handleKeyDowns);
-    document.addEventListener('keyup', handleKeyUps);
-    return () => {
-      document.removeEventListener('keydown', handleKeyDowns);
-      document.removeEventListener('keyup', handleKeyUps);
-      clearInterval(shiftInputLoop);
-    };
-  }, []);
-  //   const processShiftInputs = () => {
-  //     let shifting = false;
-  //     keyBindings.forEach((binding) => {
-  //       if (binding.type !== 'shift') return; //we are only polling keydown repeats for shifts
-  //       let extraDelay = 0;
-  //       const inputType = binding.type;
-  //       if (keysPressedRef.current[binding.key]) {
-  //         shifting = true;
-  //         extraDelay = currentlyShiftingRef.current ? 0 : CONFIG.SHIFT_DEBOUNCE;
-  //       }
-  //       //if that input type is disallowed, skip
-  //       if (!gameStateRef.current.allowedInputs[inputType]) {
-  //         return;
-  //       }
-
-  //       if (keysPressedRef.current[binding.key]) {
-  //         setGameState((prev) =>
-  //           setAllowedInput(binding.callback(prev), inputType, false)
-  //         );
-  //         setTimeout(
-  //           () => setGameState((prev) => setAllowedInput(prev, inputType, true)),
-  //           CONFIG.POLL_RATES[inputType] + extraDelay
-  //         );
-  //       }
-  //     });
-  //     //if no shift inputs were hit setting shifting to true, we arent currently shifting and reset so that we get debounce when we do
-  //     currentlyShiftingRef.current = shifting;
-  //   };
 
   //Increment game clock every tickInterval ms
   useEffect(() => {
@@ -143,6 +44,40 @@ export default function MobileApp() {
       document.body.style.overflow = 'scroll';
     };
   }, []);
+
+  const [lastShiftSteps, setLastShiftSteps] = useState(0); //how many steps from swipe origin we have shifted the current block
+  const PIXELS_PER_SHIFT_STEP = 32; //how many pixels of swiping corresponds to a side shift step
+  const handleSwipeStart = () => {
+    setLastShiftSteps(0);
+  };
+  const handleSwiping = (e: SwipeEventData) => {
+    const dx = e.deltaX;
+    const shiftSteps = Math.floor(dx / PIXELS_PER_SHIFT_STEP);
+    if (shiftSteps !== lastShiftSteps) {
+      const direction: Direction = shiftSteps > lastShiftSteps ? 'R' : 'L';
+      setGameState((prevState) => shiftBlock(prevState, direction));
+      setLastShiftSteps(shiftSteps);
+    }
+  };
+  const handleTap = () => {
+    setGameState((prevState) => rotateBlock(prevState, 'CW'));
+  };
+  const handleSwipeDown = () => {
+    setGameState((prevState) => hardDropBlock(prevState));
+  };
+  const handleSwipeUp = () => {
+    setGameState((prevState) => holdAndPopHeld(prevState));
+  };
+
+  const handlers = useSwipeable({
+    onSwiping: handleSwiping,
+    onSwipeStart: handleSwipeStart,
+    onTap: handleTap,
+    onSwipedDown: handleSwipeDown,
+    onSwipedUp: handleSwipeUp,
+    preventScrollOnSwipe: true,
+    delta: { down: 50, up: 50 },
+  });
   return (
     <div className="relative flex flex-col items-center overflow-hidden">
       {/* Top bar */}
@@ -167,11 +102,13 @@ export default function MobileApp() {
         </div>
       </div>
       {/* Main game board */}
-      <BoardDisplay
-        board={boardWithFallingBlock(gameState)}
-        cellBorderStyle={cellBorderStyles[cellBorderStyleIndex]}
-        classNames="w-[80%]"
-      />
+      <div {...handlers} className="w-[80%]">
+        <BoardDisplay
+          board={boardWithFallingBlock(gameState)}
+          cellBorderStyle={cellBorderStyles[cellBorderStyleIndex]}
+          classNames="w-full"
+        />
+      </div>
       {/* Game over modal */}
       {gameState.over && (
         <div className="absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 bg-slate-900 bg-opacity-80">
