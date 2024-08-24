76a60325fb9f37750f8a798f0c35a982bcf9fb47
Brian Smiley <briantsmiley42@gmail.com>
2024-08-23 22:28:40 -0400

    reorganizes

--- src/AppMobile.tsx
+++ src/AppMobile.tsx
@@ -102,71 +102,75 @@ export default function MobileApp() {
   });
 
   return (
-    <div
-      className="relative flex flex-col items-center overflow-hidden"
-      onContextMenu={(e) => e.preventDefault()}
-    >
-      {/* Top bar */}
-      <div className="flex w-full items-center justify-between">
-        <div className="aspect-square w-1/4 shrink-0 border-r">
-          <PieceDisplay piece={gameState.heldShape} text="Held" />
-        </div>
-        <div className="text-default flex w-full flex-col justify-between self-stretch bg-black px-2 py-2">
-          <span>
-            Score: {gameState.blocksSpawned === 0 ? '-' : gameState.score}
-          </span>
-          <span>
-            Lines:{' '}
-            {gameState.blocksSpawned === 0 ? '-' : gameState.linesCleared}
-          </span>
-          <span>
-            Level: {gameState.blocksSpawned === 0 ? '-' : gameState.level}
-          </span>
-        </div>
-        <div className="aspect-square w-1/4 shrink-0 border-l">
-          <PieceDisplay piece={gameState.shapeQueue[0]} text="Next" />
-        </div>
-      </div>
-      {/* Main game board */}
-      <div {...handlers} className="w-[300px]">
-        <BoardDisplay
-          board={boardWithFallingBlock(gameState)}
-          cellBorderStyle={cellBorderStyles[cellBorderStyleIndex]}
-          classNames="w-full"
-        />
-      </div>
-      {/* Game over modal */}
-      {gameState.over && (
-        <div className="absolute left-1/2 top-1/2 flex h-full w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center bg-slate-900 bg-opacity-80">
-          <div className="text-default flex w-full animate-flash flex-col items-center justify-center gap-5 px-8 py-4 text-5xl">
-            <span className="">GAME OVER</span>
-            <span>Score: {gameState.score}</span>
-          </div>
-          <button
-            onClick={() => setGameState(startGame(gameState))}
-            style={{ borderStyle: 'inset' }}
-            className="text-default mt-4 border-[8px] border-green-500 bg-slate-900 bg-opacity-80 px-4 py-4"
-          >
-            Restart
-          </button>
-        </div>
-      )}
-      {/* Startgame modal */}
-      {gameState.blocksSpawned === 0 && (
-        <button
-          className="text-default absolute left-1/2 top-1/2 -translate-x-1/2 border-[10px] border-green-500 bg-slate-900 bg-opacity-80 px-4 py-4"
-          style={{ borderStyle: 'inset' }}
-          onClick={() => setGameState(startGame(gameState))}
-        >
-          Start
-        </button>
-      )}
+    <>
       <div
-        className="absolute bottom-0 h-1/5 w-full bg-transparent"
+        className="relative z-20 flex flex-col items-center overflow-hidden"
+        onContextMenu={(e) => e.preventDefault()}
+      >
+        {/* Top bar */}
+        <div className="flex w-full items-center justify-between">
+          <div className="aspect-square w-1/4 shrink-0 border-r">
+            <PieceDisplay piece={gameState.heldShape} text="Held" />
+          </div>
+          <div className="text-default flex w-full flex-col justify-between self-stretch bg-black px-2 py-2">
+            <span>
+              Score: {gameState.blocksSpawned === 0 ? '-' : gameState.score}
+            </span>
+            <span>
+              Lines:{' '}
+              {gameState.blocksSpawned === 0 ? '-' : gameState.linesCleared}
+            </span>
+            <span>
+              Level: {gameState.blocksSpawned === 0 ? '-' : gameState.level}
+            </span>
+          </div>
+          <div className="aspect-square w-1/4 shrink-0 border-l">
+            <PieceDisplay piece={gameState.shapeQueue[0]} text="Next" />
+          </div>
+        </div>
+        {/* Main game board */}
+        <div {...handlers} className="w-[70%]">
+          <BoardDisplay
+            board={boardWithFallingBlock(gameState)}
+            cellBorderStyle={cellBorderStyles[cellBorderStyleIndex]}
+            classNames="w-full"
+          />
+          {/* Startgame modal */}
+          {gameState.blocksSpawned === 0 && (
+            <button
+              className="text-default absolute left-1/2 top-1/2 -translate-x-1/2 border-[10px] border-green-500 bg-slate-900 bg-opacity-80 px-4 py-4"
+              style={{ borderStyle: 'inset' }}
+              onClick={() => setGameState(startGame(gameState))}
+            >
+              Start
+            </button>
+          )}
+        </div>
+        {/* Game over modal */}
+        {gameState.over && (
+          <div className="absolute left-1/2 top-1/2 flex h-full w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center bg-slate-900 bg-opacity-80">
+            <div className="text-default flex w-full animate-flash flex-col items-center justify-center gap-5 px-8 py-4 text-5xl">
+              <span className="">GAME OVER</span>
+              <span>Score: {gameState.score}</span>
+            </div>
+            <button
+              onClick={() => setGameState(startGame(gameState))}
+              style={{ borderStyle: 'inset' }}
+              className="text-default mt-4 border-[8px] border-green-500 bg-slate-900 bg-opacity-80 px-4 py-4"
+            >
+              Restart
+            </button>
+          </div>
+        )}
+      </div>
+      <div
+        className="absolute bottom-0 z-30 h-1/5 w-full bg-transparent"
         // soft drop while touching this div
         onTouchStart={() => setSoftDropping(true)}
         onTouchEnd={() => setSoftDropping(false)}
-      ></div>
-    </div>
+        onContextMenu={(e) => e.preventDefault()}
+      />
+      <div className="absolute bottom-0 z-0 h-1/5 w-full bg-slate-500 bg-opacity-80" />
+    </>
   );
 }
