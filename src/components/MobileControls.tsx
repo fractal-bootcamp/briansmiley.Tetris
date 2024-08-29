2070426d2e889d33195a1ad3df102ae710bb4ea0
Brian Smiley <briantsmiley42@gmail.com>
2024-08-29 12:45:11 -0400

    slight restyle

--- src/components/MobileControls.tsx
+++ src/components/MobileControls.tsx
@@ -12,12 +12,12 @@ export default function MobileControls({}: MobileControlsProps) {
   return (
     <div
       onClick={(e) => e.stopPropagation()}
-      className={`text-default border-outset flex w-full flex-col items-center justify-center gap-2 bg-slate-900 p-3`}
+      className={`text-default border-outset flex w-[90%] flex-col items-center justify-center gap-2 bg-slate-900 p-3`}
     >
       <div className="mb-2 text-2xl font-bold underline">Controls</div>
-      <div className="flex w-full flex-col gap-4">
+      <div className="flex w-full flex-col gap-4 text-lg">
         <div className="flex items-center justify-between gap-1">
-          <div className="flex min-w-24 flex-row items-center justify-center">
+          <div className="flex min-w-[50%] flex-row items-center justify-center">
             <ArrowLeft />
             <Fingerprint />
             <ArrowRight />
@@ -26,27 +26,27 @@ export default function MobileControls({}: MobileControlsProps) {
         </div>
 
         <div className="flex items-center justify-between gap-1">
-          <div className="flex min-w-24 flex-col items-center justify-center">
+          <div className="flex min-w-[50%] flex-col items-center justify-center">
             <ArrowUp />
             <Fingerprint />
           </div>
           Hold / Swap
         </div>
         <div className="flex items-center justify-between gap-1">
-          <div className="itc-row flex min-w-24 items-center justify-center gap-1">
+          <div className="itc-row flex min-w-[50%] items-center justify-center gap-1">
             <Fingerprint /> Tap
           </div>
           <RotateCw />
         </div>
         <div className="flex items-center justify-between gap-1">
-          <div className="flex min-w-24 flex-col items-center justify-center">
+          <div className="flex min-w-[50%] flex-col items-center justify-center">
             <Fingerprint />
             <ArrowDown />
           </div>
           Hard drop
         </div>
         <div className="flex items-center justify-between gap-1">
-          <div className="flex min-w-24 flex-col items-center justify-center gap-2 text-center">
+          <div className="flex min-w-[50%] flex-col items-center justify-center gap-2 text-center">
             Tap + hold
             <br />
             bottom region
