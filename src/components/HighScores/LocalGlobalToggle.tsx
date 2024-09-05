ed3c0387c1854c5c635e6f5571f7392f1d07d030
Brian Smiley <briantsmiley42@gmail.com>
2024-09-05 02:25:44 -0400

    adds more imagery to localglobal switch toggle

--- src/components/HighScores/LocalGlobalToggle.tsx
+++ src/components/HighScores/LocalGlobalToggle.tsx
@@ -9,14 +9,31 @@ export default function LocalGlobalToggle({
   toggleGlobal,
 }: LocalGlobalToggleProps) {
   const displayClass = (global: boolean) =>
-    global === displayGlobal ? 'text-white' : 'text-slate-500';
+    global === displayGlobal ? 'text-white' : 'text-slate-400';
+  const switchStyle: React.CSSProperties = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    transition: 'transform 0.5s',
+    transform: displayGlobal ? 'translateX(calc(45%))' : 'translateX(-45%)',
+    zIndex: '20',
+    width: '56px',
+    height: '28px',
+  };
   return (
     <button
       onClick={toggleGlobal}
-      className="flex h-6 w-12 items-center justify-between"
+      className="relative flex h-7 items-center justify-between gap-3 overflow-hidden rounded-full bg-slate-300 px-1"
     >
-      <Computer size={20} className={`${displayClass(false)}`} />
-      <Globe size={20} className={`${displayClass(true)}`} />{' '}
+      <div className="rounded-full bg-slate-500" style={switchStyle} />
+      <Computer
+        size={18}
+        className={`${displayClass(false)} z-30 transition-all duration-500`}
+      />
+      <Globe
+        size={18}
+        className={`${displayClass(true)} z-30 transition-all duration-500`}
+      />{' '}
     </button>
   );
 }
